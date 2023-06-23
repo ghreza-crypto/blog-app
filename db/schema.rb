ce69208fdb99ad5444e5dee10837bd4b897195cb
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-06-23 11:29:10 +0430

    Create likes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_065519) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_065814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_065519) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -46,5 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_065519) do
 
   add_foreign_key "comments", "posts", on_delete: :cascade
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts", on_delete: :cascade
+  add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
