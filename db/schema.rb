7b7a8fc8cad4a18176caf333dcc4b57d9f53cf44
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-06-23 11:17:32 +0430

    Create posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_054841) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_064104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "author_id"
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_054841) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users", column: "author_id"
 end
