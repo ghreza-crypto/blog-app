2121945ef4c8921605ade5bf8668367641b8b655
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-06-24 20:15:47 +0430

    Fix linter errros

--- Gemfile
+++ Gemfile
@@ -67,8 +67,8 @@ end
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem 'capybara'
+  gem 'rspec-rails'
   gem 'selenium-webdriver'
   gem 'webdrivers'
-  gem 'rspec-rails'
 end
 gem 'rubocop', '>= 1.0', '< 2.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
-
   def recent_posts
     Post.where(author_id: id).order(created_at: :desc).limit(3)
   end
