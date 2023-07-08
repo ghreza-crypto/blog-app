49d3d021c2ffa875ac7e8dcd860180db4dbb5f4c
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-07-08 19:13:03 +0430

    Add v1 api structure with users controller for api

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::UsersController < ApplicationController
+    def index
+      @users = User.all
+  
+      respond_to do |format|
+        format.json { render json: @users, status: 200 }
+      end
+    end
+  
+    def show
+      if params[:id] == 'sign_out'
+        sign_out current_user
+        redirect_to new_user_session_path, notice: 'Sign out succesfully' unless @user
+        format.json { render status: 200 }
+      else
+        @user = User.find_by(id: params[:id])
+        respond_to do |format|
+          redirect_to users_path, notice: 'User found' unless @user
+          format.json { render json: @user, status: 200 }
+        end
+      end
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,13 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'users/sign_out', to: 'devise/sessions#destroy'
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :users, only: [:index, :show] do
     resources :posts do
       resources :comments, only: [:new, :create,:destroy]
       resources :likes, only: [:new, :create]
     end
   end
+  
   root 'users#index'
   get '/users', to: 'users#index'
 end
